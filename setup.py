ee06f936505a8a129a7a9ceb9acf74e45af2b621
Bryan Dickinson <bd_627@yahoo.com>
2021-05-10 11:15:01 -0700

    add proroject template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Determining mobile farmers market locations to address food deserts based on public transportation ridership.',
+    author='DS4A-team92',
+    license='',
+)
